5146f45433f8091b1f55d0ae35bc2c8e88b1f8f5
Knuxxes <specter_hikaru@hotmail.com>
2015-12-15 09:47:22 +0100

    Initial commit

---
+++ src/main/java/practicastorm/App.java
@@ -0,0 +1,13 @@
+package practicastorm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
